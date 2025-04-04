87608363aca3aa2dc06a32e63877b783c706a29a
Biel Rabasa <bielrabasa@gmail.com>
2025-04-04 19:31:16 +0200

    Rules Scene Preparated

--- Assets/_WFC_TOOL/Scripts/SBO_Rules.cs
+++ Assets/_WFC_TOOL/Scripts/SBO_Rules.cs
@@ -5,7 +5,7 @@ namespace PCG_Tool
     [CreateAssetMenu(fileName = "New Rules", menuName = "WFC_Tool/Rules")]
     public class SBO_Rules : ScriptableObject
     {
-
+        public SBO_TileSet tileSet;
     }
 
 }
---
+++ Assets/_WFC_TOOL/Utilities/SCR_InspectorLockUtility.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Reflection;
+using UnityEditor;
+
+public static class SCR_InspectorLockUtility
+{
+    public static void SetInspectorLock(bool isLocked)
+    {
+        var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+        var windows = Resources.FindObjectsOfTypeAll(inspectorType);
+
+        foreach (var window in windows)
+        {
+            var property = inspectorType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            property?.SetValue(window, isLocked, null);
+
+            // Forzar el repaint
+            var repaint = inspectorType.GetMethod("Repaint", BindingFlags.Instance | BindingFlags.Public);
+            repaint?.Invoke(window, null);
+        }
+    }
+}
